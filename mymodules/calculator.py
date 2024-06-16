c8d955755d1cfd2627e7e3df7a6536058b294875
254In61 <allanmaseghe4@gmail.com>
2024-06-16 15:52:16 +1000

    Updates @ Sun 16 Jun 2024 15:52:16 AEST

--- mymodules/calculator.py
+++ mymodules/calculator.py
@@ -1,24 +1,27 @@
-# Function for multiplying
+"""
+This module provides basic functions to call when testing Jenkins cicd pipeline deployment.
+The code is as simple as possible to achieve the objective.
+"""
+
+
 def multiply(first_number, second_number):
     """Function multiplies 2 numbers and returns the result."""
     return first_number * second_number
 
 
-# Function for division
 def divide(first_number, second_number):
-    """Function divides a number with the other returns the divide result. Also error checks for zero denominator"""
+    """Function divides a number with the other returns the divide result.
+    Also error checks for zero denominator"""
     if second_number == 0:
         raise ValueError("Cannot divide by zero")
     return first_number / second_number
 
 
-# Function for addition
 def adding(first_number, second_number):
     """Function add numbers and returns the value"""
     return first_number + second_number
 
 
-# Function for subtraction
 def subtract(first_number, second_number):
     """Function subtracts one number from the other and returns the value"""
     return first_number - second_number
